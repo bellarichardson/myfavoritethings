fd673b039f565c0190d289b2bd1cc25dc796d91e
bellarichardson <bcrichardson@dons.usfca.edu>
2025-05-01 14:24:23 -0700

    update

--- js/sketch.js
+++ js/sketch.js
@@ -8,7 +8,7 @@ const amplitude = 30;
 const verticalLetterSpacing = 10;
 
 function preload(){
-  hills = loadImage('hills.png');
+  hills = loadImage('../img/hills.png');
 }
 
 function windowResized(){
